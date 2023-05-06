4c4a98aee02cbcc56a38c7952f4f182d3840de82
Joe <Joe@DESKTOP-REE2A5O>
2023-05-06 14:41:25 +0900

    start up

---
+++ src/main/java/com/example/demo/BowwowApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowwowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowwowApplication.class, args);
+	}
+
+}
